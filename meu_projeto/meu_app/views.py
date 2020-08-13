1f367f588c4b369071e177411b79409205669310
Vinicius Warlet <vwarlet@gmail.com>
2020-08-13 18:53:06 -0300

    CRUD de Eventos

--- meu_projeto/meu_app/views.py
+++ meu_projeto/meu_app/views.py
@@ -32,3 +32,42 @@ def lista_eventos(request):
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
 
+@login_required(login_url='/login/')
+def evento(request):
+    id_evento = request.GET.get('id')
+    dados = {}
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        id_evento = request.POST.get('id_evento')
+        '''
+        # outro modo de fazer as alterações
+        evento = Evento.objects.get(id=id_evento)
+        if evento.usuario == usuario:
+            evento.titulo = titulo
+            evento.descricao = descricao
+            evento.data_evento = data_evento
+            evento.save()
+        '''
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo = titulo, data_evento = data_evento, descricao = descricao)
+        else: 
+            Evento.objects.create(titulo = titulo, data_evento = data_evento, descricao = descricao, usuario = usuario)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    # usuario logado só pode excluir os seus eventos
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
